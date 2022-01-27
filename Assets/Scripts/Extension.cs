c23a3ffbf352cbfc7b51bf69a6f7a1519d8507fa
Tom Choi <jw95@live.cn>
2022-01-27 17:01:28 -0500

    title screen prototype finished (almost)

--- Assets/Scripts/Extension.cs
+++ Assets/Scripts/Extension.cs
@@ -3,17 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-enum TitleScreenOptions
+public enum TitleScreenOptions
 {
     Start,
     Option,
     Quit
 }
 
+public enum OptionScreenOptions
+{
+    Music,
+    SFX,
+    OK
+}
+
+static class Settings {
+    public static float music = 50;
+    public static float SFX = 50;
+}
+
+
 static class Constants {
-    public static Vector2[] titleScreenOptionPositions = { new Vector3(-866, -335,0),
-                                                    new Vector3(-267, -335,0),
+    public static Vector3[] titleScreenOptionPositions = { new Vector3(-866, -335,0),
+                                                    new Vector3(-266, -335,0),
                                                     new Vector3(336, -335,0)};
+    public static Vector3 optionScreenBackButtonPosition = new Vector3(-260, -345, -0);
+
+    public static Vector2 volumeSliderXRange = new Vector2(-158, 358);
 }
 
 static class Extension 
@@ -30,5 +46,16 @@ static class Extension
         int newValue = (int)(object)option - 1;
         if (newValue < 0) { newValue = Enum.GetNames(typeof(T)).Length - 1; }
         return (T)(object)newValue;
+    }
+
+    public static float Bounds(this float num, float min, float max) {
+        float output = num;
+        if (num < min) {
+            output = min;
+        }
+        if (num > max) {
+            output = max;
+        }
+        return output;
     }
 }
